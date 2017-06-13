d843b46846950745a0a1c0fe82b6efcc03053eb3
ToDDdi <kolokolnikovkm@gmail.com>
2017-06-13 13:51:03 +0700

    Update frequency.h

--- src/frequency.h
+++ src/frequency.h
@@ -10,8 +10,10 @@ struct words
 };
 
 struct words *bintree(struct words **head,char *word);//bintree is a function of type struct words and the 1st arg is a pointer to the bin tree that stores the words
-void print_tree(struct words *head);//print_tree is a function that prints a binary tree
+void print_tree(FILE *fout, struct words *head, int total);//print_tree is a function that prints a binary tree
 char *readfile(char *filename);//readfile is a function that reads a text file and stores it in a string
+void hash_tab_add(int id, char *data);
+void print_table(FILE *fout, int total);
 int total;//an interger to store the total number of words in the file
 
 #endif
